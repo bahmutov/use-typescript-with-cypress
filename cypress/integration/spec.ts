c3adece60eb3265a7342f0301b28cf43b2c3b170
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-01-19 09:24:02 -0500

    proxy test and es6 target

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -4,3 +4,18 @@ it('loads examples', () => {
   cy.visit(url)
   cy.contains('Kitchen Sink')
 })
+
+it('handles ES6 features', () => {
+  const o = {
+    // @ts-ignore
+    name: cy.stub().returns('Joe')
+  }
+  const proxy = new Proxy(o, {
+    get (target, method) {
+      return target.name
+    }
+  })
+  // @ts-ignore
+  expect(proxy.whatever()).to.equal('Joe')
+  expect(o.name).to.have.been.calledOnce
+})
